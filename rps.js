be6ec06cfb3a3eaf82e57d649aa6c46746d92880
Jacob Foggie <jacob.foggie@outlook.com>
2023-03-30 18:03:52 +0100

    Create function for computer picking random choice in rps

--- rps.js
+++ rps.js
@@ -1,1 +1,4 @@
-console.log("5");
+const choice = ["rock", "paper", "scissors"];
+
+const getComputerChoice = Math.floor(Math.random() * choice.length);
+console.log(getComputerChoice, choice[getComputerChoice]);
