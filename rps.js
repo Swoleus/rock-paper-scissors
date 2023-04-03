11fa0e5ccfcefc26931b036ee16f07a3edca2649
Jacob Foggie <jacob.foggie@outlook.com>
2023-04-03 18:58:04 +0100

    Create prompt for player selection. Create function for playing a single round. Take selections and return string for parameters

--- rps.js
+++ rps.js
@@ -8,4 +8,22 @@ function getComputerChoice() {
     return choice[Math.floor(Math.random() * choice.length)]
 }
 
-console.log(getComputerChoice());
+/* console.log(getComputerChoice()); */
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `It is a tie, you both picked ${playerSelection}!`
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return `You win! Rock beats scissors!`
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return `You win! Paper beats rock!`
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return `You win! Scissors beats paper!`
+    } else {
+        return `Loser! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+const playerSelection = prompt("Pick 'Rock, Paper or Scissors").toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
