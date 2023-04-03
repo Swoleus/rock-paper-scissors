839cd5342905430c711a49bced01140547407197
Jacob Foggie <jacob.foggie@outlook.com>
2023-04-03 20:13:35 +0100

    Create new function to store score values and results message

--- rps.js
+++ rps.js
@@ -10,7 +10,7 @@ function getComputerChoice() {
 
 /* console.log(getComputerChoice()); */
 
-function playRound(playerSelection, computerSelection) {
+/* function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return `It is a tie, you both picked ${playerSelection}!`
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
@@ -22,7 +22,51 @@ function playRound(playerSelection, computerSelection) {
     } else {
         return `Loser, ${computerSelection} beats ${playerSelection}`;
     }
+} This is to demonstrate 1 round*/ 
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            computerScore++;
+            return lose;
+        } else if (computerSelection === "rock") {
+            return tie;
+        } else {
+            userScore++;
+            return win;
+        }
+    }
+
+    if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            computerScore++;
+            return lose;
+        } else if (computerSelection === "scissors") {
+            return tie;
+        } else {
+            userScore++
+            return win;
+        }
+    }
+
+    if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            computerScore++;
+            return lose;
+        } else if (computerSelection === "paper") {
+            return tie;
+        } else {
+            userScore++;
+            return win;
+        }
+    }
 }
+
+let userScore = parseInt(0);
+let computerScore = parseInt(0);
+let win = "You win!";
+let lose = "You lose!";
+let tie = "It's a tie!";
 
 const playerSelection = prompt("Pick 'Rock, Paper or Scissors").toLowerCase();
 const computerSelection = getComputerChoice();
